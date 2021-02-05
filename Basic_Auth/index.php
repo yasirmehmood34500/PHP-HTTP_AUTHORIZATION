3dd04c8a03aae4832521890a023abc463e263b20
Yasir Memood <yasirmehmood34500@gmail.com>
2021-02-05 19:09:27 +0500

    api res

--- Basic_Auth/index.php
+++ Basic_Auth/index.php
@@ -1,30 +1,44 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *'); 
 header("Content-Type: application/json; charset=UTF-8");
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="Required"');
-    header('HTTP/1.0 401 Unauthorized');
-    http_response_code(401);
-    echo json_encode(array(
-        "status" => 401,
-        "response" => 'User Auth Required'
-    ));
-    exit;
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS'); 
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+$i=1;
+if ($i==1) {
+	if (!isset($_SERVER['PHP_AUTH_USER'])) {
+	    header('WWW-Authenticate: Basic realm="User Auth Required"');
+	    header('HTTP/1.0 401 Unauthorized');
+	    http_response_code(401);
+	    echo json_encode(array(
+	        "status" => 401,
+	        "response" => 'User Auth Required'
+	    ));
+	    exit;
+	} else {
+	    if ($_SERVER['PHP_AUTH_USER'] == "yasir" && $_SERVER['PHP_AUTH_PW'] == "123") {
+	        http_response_code(200);
+	        echo json_encode(array(
+	            "status" => 200,
+	            "response" => 'Successfully Conntect',
+	            "username" => $_SERVER['PHP_AUTH_USER'],
+	            "password" => $_SERVER['PHP_AUTH_PW']
+	        ));
+	    } else {
+	        http_response_code(404);
+	        echo json_encode(array(
+	            "status" => 404,
+	            "response" => 'Invalid Auth try again!'
+	        ));
+	    }   
+	}
 } else {
-    if ($_SERVER['PHP_AUTH_USER'] == "yasir" && $_SERVER['PHP_AUTH_PW'] == "123") {
-        http_response_code(200);
-        echo json_encode(array(
+	echo json_encode(array(
             "status" => 200,
-            "response" => 'Successfully Conntect',
-            "username" => $_SERVER['PHP_AUTH_USER'],
-            "password" => $_SERVER['PHP_AUTH_PW']
+            "response" => 'Without Auth Username & Password'
         ));
-    } else {
-        http_response_code(404);
-        echo json_encode(array(
-            "status" => 404,
-            "response" => 'Invalid Auth try again!'
-        ));
-    }   
 }
+
+
+
+
  ?>
--- CORS_Allow/index.php
+++ CORS_Allow/index.php
@@ -1,7 +1,6 @@
 <?php
-
-// send some CORS headers so the API can be called from anywhere
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
 
 echo json_encode(array("resp"=> "success"));
